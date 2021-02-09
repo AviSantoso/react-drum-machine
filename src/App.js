d724f16840cb401c68e65909b0a4f2edb23d1258
Aviciena Santoso <avi.santoso@gmail.com>
2021-02-09 11:30:31 +0800

    keep working on styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import "./styles.css";
 
 export default function App() {
-  const padButtons = [...Array(9).keys()];
+  const padButtons = [
+    { key: "Q" },
+    { key: "W" },
+    { key: "E" },
+    { key: "A" },
+    { key: "S" },
+    { key: "D" },
+    { key: "Z" },
+    { key: "X" },
+    { key: "C" }
+  ];
   return (
     <div id="root">
-      <div id="app">
+      <div id="drum-machine">
+        <div id="display"></div>
         <div id="pad-container">
           {padButtons.map((x) => (
-            <div className="pad" id={"pad" + x}></div>
+            <button className="drum-pad" id={"pad" + x.key} key={x.key}>
+              {x.key}
+            </button>
           ))}
         </div>
       </div>
